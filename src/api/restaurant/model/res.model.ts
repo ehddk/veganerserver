98d3c53d92c03306521c39e973d2fd4cce301f33
ehddk <bj9147@naver.com>
2025-10-19 18:00:27 +0900

    fix: dbModel 매핑 상수 수정

--- src/api/restaurant/model/res.model.ts
+++ src/api/restaurant/model/res.model.ts
@@ -22,18 +22,31 @@
 //     category?: string; // COB_CODE_NM
 //   };
 
-const mapOpenApiToDbModel = (apiRecord: any): Omit<IRestaurant, "id"> => {
-  if (!apiRecord.TYPE || !apiRecord.START_INDEX || !apiRecord.END_INDEX) {
-    throw new Error("openapi record missing required fields");
-  }
+export const mapOpenApiToDbModel = (
+  apiRecord: any
+): Omit<IRestaurant, "id"> => {
+  // if (!apiRecord.TYPE || !apiRecord.START_INDEX || !apiRecord.END_INDEX) {
+  //   throw new Error("openapi record missing required fields");
+  // }
+
+  const parseCoordinate = (coord: any): number => {
+    // null, undefined, 빈 문자열 ("") 인지 확인
+    if (!coord || String(coord).trim() === "") {
+      return 0; // 또는 -1, 혹은 DB 컬럼이 NULL 허용 시 null로 처리
+    }
+    // parseFloat()으로 변환
+    return parseFloat(coord);
+  };
+
   return {
     //db 컬럼명 : api 필드명
-    upsoName: apiRecord.UPSO_NM,
-    rdnCode: apiRecord.RDN_CODE_NM,
-    rdnDetailAddr: apiRecord.RDN_DETAIL_ADDR,
-    ctfcGbnName: apiRecord.CRTFC_GBN_NM,
-    latitude: parseInt(apiRecord.Y_DNTS),
-    longitude: parseInt(apiRecord.X_DNTS),
+    upso_name: apiRecord.UPSO_NM,
+    rdn_code: apiRecord.RDN_CODE_NM || "",
+    rdn_detail_addr: apiRecord.RDN_DETAIL_ADDR || "",
+    source_id: String(apiRecord.CRTFC_UPSO_MGT_SNO),
+    ctfc_gbn_name: apiRecord.CRTFC_GBN_NM || "",
+    latitude: parseCoordinate(apiRecord.Y_DNTS),
+    longitude: parseCoordinate(apiRecord.X_DNTS),
     category: apiRecord.BIZCND_CODE_NM,
     source_type: "OPEN_API",
   };
