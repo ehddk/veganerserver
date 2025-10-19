6d639f0dd486cd8c6a736c9f3173ed5730ed992a
ehddk <bj9147@naver.com>
2025-10-19 21:50:52 +0900

    feat: db애 저장 관련 리포지토리 인터페이스 생성

--- src/api/restaurant/repository/res.repository.ts
+++ src/api/restaurant/repository/res.repository.ts
@@ -1,5 +1,6 @@
 export interface RestuarantRepository {
-  findAll(): Promise<IRestaurant>;
+  findAll(): Promise<IRestaurant[]>;
   findById(id: string): Promise<IRestaurant | null>;
   save(restaurant: Omit<IRestaurant, "id">): Promise<IRestaurant>;
+  saveBatch(restaurant: IRestaurant[]): Promise<void>;
 }
