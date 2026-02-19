e9208689425aeb1c922f3790b88c1e3ba541666e
ehddk <bj9147@naver.com>
2026-02-19 22:37:14 +0900

    feat:검색어쿼리에 '식당'추가

--- src/utils/imageCrawler.ts
+++ src/utils/imageCrawler.ts
@@ -1,6 +1,6 @@
 export async function crawlImages(upsoName: string): Promise<string[]> {
   const images = await fetch(
-    `https://openapi.naver.com/v1/search/image?query=${upsoName}&display=2&start=1&sort=sim`,
+    `https://openapi.naver.com/v1/search/image?query=${upsoName}+식당&display=2&start=1&sort=sim`,
     {
       headers: {
         "X-Naver-Client-Id": process.env.NAVER_CLIENT_ID || "",
